f0e17a23102960f583b391a2b79a04b171ed92d0
ax2 <alexkurosakimh3@gmail.com>
2018-04-07 19:43:13 -0400

    fix database

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ app.config['CAS_AFTER_LOGIN'] = '/'
 cas = CAS(app)
 bootstrap = Bootstrap(app)
 
-dummy = "mongodb://heroku_jrmxs801:d8soja6ng9gdlfskchmi51b554@ds237979.mlab.com:37979/heroku_jrmxs801"
+dummy = "mongodb://heroku_cqcm2pgv:nd8cpu61p161k9ltshhpopu5oe@ds237989.mlab.com:37989/heroku_cqcm2pgv"
 MONGODB_URI = os.getenv('MONGODB_URI') or dummy
 app.config['MONGODB_SETTINGS'] = {
     'db': 'users',
