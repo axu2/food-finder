23e1197fb6cfc7faf98d0408ca912459b0594f02
ax2 <alexkurosakimh3@gmail.com>
2018-04-10 16:17:10 -0400

    add breakfast lunch dinner

--- app/compose.py
+++ app/compose.py
@@ -3,18 +3,26 @@ def compose_email(matches):
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
             'Saturday', 'Sunday']
 
+    meal = ["Breakfast", "Lunch", "Dinner"]
+
     days = [day + ": \n" for day in days]
     now = datetime.now()
     lastDate = now.weekday()
     week = [days[(lastDate + i) % 7] for i in range(7)]
 
-    text = "Your weekly dinner meals digest:\n\n"
-    for i, day in enumerate(matches):
-        if day:
-            text += week[i]
-            for food, hall in day:
-                text += food + " in " + hall + "\n"
-            text += "\n"
+    text = "Your weekly meals digest:\n\n"
+    for j in range(7):
+        for i in range(3):
+            day = matches[i][j]
+
+            if day:
+                text += week[i]
+                text += "\n"
+                text += meal[i] + ": \n"
+                for food, hall in day:
+                    text += food + " in " + hall + "\n"
+                text += "\n"
+
     text += "Best,\n\nAlex\n\nhttp://menu-alerts.herokuapp.com/"
 
     return text
@@ -33,12 +41,14 @@ def getMatches(user):
     dinnerListList = l[2]
 
     halls = ['Butler', 'CJL', 'Whitman', 'Roma', 'Forbes', 'Grad']
-    matches = [[] for i in range(7)]
-    for j, dinnerList in enumerate(dinnerListList): #each day
-        for i, hall in enumerate(dinnerList): #each hall
-            for food in hall:
-                for pref in user.prefs:
-                    if pref.lower() in food['item'].lower():
-                        matches[j].append((food['item'], halls[i]))
+    matches = [[[] for i in range(7)] for j in range(3)]
+
+    for k, mealListList in enumerate(l):
+        for j, mealList in enumerate(mealListList): #each day
+            for i, hall in enumerate(mealList): #each hall
+                for food in hall:
+                    for pref in user.prefs:
+                        if pref.lower() in food['item'].lower():
+                            matches[k][j].append((food['item'], halls[i]))
 
     return matches
